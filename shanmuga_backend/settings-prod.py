3bfbfba6fd97727c3318ce1a47108fa07020d767
SMRFT <110091550+SMRFT@users.noreply.github.com>
2025-10-07 10:39:14 +0530

    Update settings-prod.py

--- shanmuga_backend/settings-prod.py
+++ shanmuga_backend/settings-prod.py
@@ -123,6 +123,12 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "https://chc.shinova.in",
+]
+
 CORS_ALLOW_ALL_ORIGINS = True
 
 
@@ -135,6 +141,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 
 
