fd249dbdaa27b0c826f7e6fba93f06ae2ee38a01
SMRFT <110091550+SMRFT@users.noreply.github.com>
2025-10-07 10:43:09 +0530

    Update settings-prod.py

--- shanmuga_backend/settings-prod.py
+++ shanmuga_backend/settings-prod.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',  
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,8 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
+
 
 ROOT_URLCONF = 'shanmuga_backend.urls'
 
@@ -129,7 +130,6 @@ CORS_ALLOWED_ORIGINS = [
     "https://chc.shinova.in",
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
 
 
 # Static files (CSS, JavaScript, Images)
@@ -141,6 +141,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 
 
