17fbc808e709197c1b0c03cf62b41bced2ab87ca
SMRFT <110091550+SMRFT@users.noreply.github.com>
2026-01-14 12:37:28 +0530

    Update models.py

--- core/models.py
+++ core/models.py
@@ -260,9 +260,9 @@ class HmspatientBilling(AuditModel):
     age_type = models.CharField(max_length=10, blank=True)
     gender = models.CharField(max_length=10)
     phone = models.CharField(max_length=15, blank=True)
-    opiptype=models.CharField(max_length=15, blank=True)
     location_id=models.CharField(max_length=15, blank=True,default="hms")
     billnumber= models.CharField(max_length=15, blank=True,primary_key=True)
+    IPOPType=models.CharField(max_length=15, blank=True)
     date=models.DateTimeField()
     ref_doctor= models.CharField(max_length=500, blank=True)
     testdetails = models.JSONField(blank=True, null=True)
@@ -304,4 +304,5 @@ class CommunicationLog(AuditModel):
     
     def __str__(self):
         return f"{self.type} to {self.recipient}"
+
 
