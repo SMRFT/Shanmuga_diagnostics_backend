fdc74e4ff377bdca93fb10e6b3680a755c8b61ac
Parthiban-backend-dev <parthipan3121461@gmail.com>
2026-02-23 21:35:58 +0530

    update hmsbarcode

--- core/Views/hms/hmsbarcode.py
+++ core/Views/hms/hmsbarcode.py
@@ -15,11 +15,11 @@ def save_hms_barcodes(request):
     if request.method == "POST":
         try:
             data = request.data
-            billnumber = data.get('billnumber') or data.get('billnumber')
+            barcode = data.get('barcode') or data.get('barcode')
 
-            # Check if bill_no already exists
-            if Hmsbarcode.objects.filter(billnumber=billnumber).exists():
-                return JsonResponse({'error': 'Bill number already exists!'}, status=400)
+            # Check if barcode already exists
+            if Hmsbarcode.objects.filter(barcode=barcode).exists():
+                return JsonResponse({'error': 'Barcode already exists!'}, status=400)
 
             # Extract employee ID from request
             employee_id = data.get('auth-user-id')
