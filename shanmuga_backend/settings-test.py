f13b5c7f94e4241be121398e347546a7d2481d65
SMRFT <110091550+SMRFT@users.noreply.github.com>
2026-07-15 12:32:58 +0530

    Update settings-test.py

--- shanmuga_backend/settings-test.py
+++ shanmuga_backend/settings-test.py
@@ -124,7 +124,7 @@ TIME_ZONE = 'Asia/Kolkata'  # Set to IST
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
-
+CORS_ALLOW_ALL_ORIGINS = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
